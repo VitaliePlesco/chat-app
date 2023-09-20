685be0bec05cb9d5d7add36bb1f4edcbb26976b3
Vitalie Plesco <v.plesco@gmail.com>
2023-09-20 22:58:10 +0300

    created generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
+const generateMessage = require("./utils/message");
 
 const publicPath = path.join(__dirname, "../client");
 const port = process.env.PORT || 3000;
@@ -16,24 +17,18 @@ io.on("connection", (socket) => {
 
   socket.on("createMessage", (message) => {
     console.log("createMessage", message);
-    io.emit("newMessage", {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime(),
-    });
+    io.emit("newMessage", generateMessage(message.from, message.text));
   });
 
-  socket.emit("newMessage", {
-    from: "Admin",
-    text: "Welcome to the chat app",
-    createdAt: new Date().getTime(),
-  });
+  socket.emit(
+    "newMessage",
+    generateMessage("Admin", "Welcome to the chat app")
+  );
 
-  socket.broadcast.emit("newMessage", {
-    from: "Admin",
-    text: "New user joined",
-    createdAt: new Date().getTime(),
-  });
+  socket.broadcast.emit(
+    "newMessage",
+    generateMessage("Admin", "New user joined")
+  );
 
   socket.on("disconnect", () => {
     console.log("User was disconnected");
